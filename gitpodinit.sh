218b63816fa392c60dc1334979c119e812f89cfd
sidpalas <sid.palas@gmail.com>
2022-08-10 13:23:30 +0000

    move bashrc modification to before task

--- gitpodinit.sh
+++ gitpodinit.sh
@@ -6,5 +6,5 @@ HUGO_TAR_FILE=hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
 
 wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${HUGO_TAR_FILE}
 tar -xf ${HUGO_TAR_FILE}
-sudo mv hugo /usr/local/bin/
+mv hugo /workspace
 rm ${HUGO_TAR_FILE}
